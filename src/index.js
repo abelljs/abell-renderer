73b066a02a3371ca929b1c687042ed25915b9b3b
Saurabh Daware <saurabhdaware99@gmail.com>
2020-05-01 22:06:22 +0530

    prettfied the prototype

--- src/index.js
+++ src/index.js
@@ -2,25 +2,27 @@ const vm = require('vm');
 
 const sandbox = {
   $contentList: [
-    {
-      name: 'Nice'
-    },
-    {
-      name: 'very nice'
-    }
+    {name: 'Nice'},
+    {name: 'very cool'}
   ],
   output: ''
 };
 
 
-const template = `output = $contentList.map(content => {
-  return '<div>' + content.name + '</div>';
-}).join('')`
+const template = `
+output = $contentList
+  .map(content => 
+    \`<div>\${content.name}</div>\`
+  )
+  .join('\\n')
+`
 
 const script = new vm.Script(template);
 
 const context = new vm.createContext(sandbox);
 script.runInContext(context);
 
-
-console.log(sandbox);
+console.log(sandbox.output);
+// outputs
+// <div>Nice</div>
+// <div>very cool</div>

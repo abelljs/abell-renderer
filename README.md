17c9cc03f644ae2c1ed3b2e7a340a15eb73e11f0
Saurabh Daware <saurabhdaware99@gmail.com>
2020-05-01 21:44:40 +0530

    intial prototype with vm

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # abell-renderer
-A wrapper arround Mustache that adds some additional features and some syntatic sugar required for abell
 
 NOT READY FOR YET
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-function render(template, view) {
-  return Mustache.render(template, view);
-}
+const vm = require('vm');
+
+const sandbox = {
+  $contentList: [
+    {
+      name: 'Nice'
+    },
+    {
+      name: 'very nice'
+    }
+  ],
+  output: ''
+};
+
+
+const template = `output = $contentList.map(content => {
+  return '<div>' + content.name + '</div>';
+}).join('')`
+
+const script = new vm.Script(template);
+
+const context = new vm.createContext(sandbox);
+script.runInContext(context);
+
+
+console.log(sandbox);

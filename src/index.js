4dc63305bdde937cad22c3a0765cf7d7f27243b9
Saurabh Daware <saurabhdaware99@gmail.com>
2020-05-02 11:08:52 +0530

    description added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const vm = require('vm');
 
+/**
+ * Executes the JavaScript code from string
+ * 
+ * @param {string} jsToExecute - JavaScript code in String to parse and execute
+ * @param {any} sandbox - The variables and all the information required by program
+ */
 function execute(jsToExecute, sandbox) {
   const script = new vm.Script('output = ' + jsToExecute);
 
@@ -9,8 +15,14 @@ function execute(jsToExecute, sandbox) {
   return sandbox.output;
 }
 
-// User's context
 
+/**
+ * Captures groups from regex and executes RegEx.exec() function on all.
+ * 
+ * @param {regex} regex - Regular Expression to execute on.
+ * @param {string} template - HTML Template in string.
+ * 
+ */
 const execRegexOnAll = (regex, template) => {
   const allMatches = [];
   let match = regex.exec(template);
@@ -26,6 +38,13 @@ const execRegexOnAll = (regex, template) => {
 }
 
 
+/**
+ * Outputs vanilla html string when abell template and sandbox is passed.
+ * 
+ * @param {string} abellTemplate - String of Abell File.
+ * @param {any} sandbox - Object of variables. The template will be executed in context of this sandbox.
+ * 
+ */
 function render(abellTemplate, sandbox) {
   const {matches, input} = execRegexOnAll(/{{(.*?)}}/gs, abellTemplate)
   let renderedHTML = '';

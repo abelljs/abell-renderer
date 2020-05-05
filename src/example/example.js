2c474a3213523337e7a9e9f38573bc6111149da3
Pika1998 <prafzzraichurkar1@gmail.com>
2020-05-05 14:40:48 +0530

    Added Comments in index.js and example.js to simplify the process for developers

--- src/example/example.js
+++ src/example/example.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const abellRenderer = require('../index.js');
 
+// Sandox holds parameters values are used in abell file
 const sandbox = {
   $contentList: [
     {name: 'Nice'},
@@ -14,11 +15,12 @@ const sandbox = {
   }
 };
 
-const htmlTemplate = abellRenderer.render(
+// Transforms the abell file into HTML and stores in variable htmlTemplate
+const htmlTemplate = abellRenderer.render(  
   fs.readFileSync(path.join(__dirname, 'index.abell')), 
   sandbox
 );
-
+// The transformed abell file content stored in htmlTemplate is written to index.html 
 fs.writeFileSync(path.join(__dirname, 'index.html'), htmlTemplate);
 
 console.log("🎉 Output generated at src/example/index.html 🌻");

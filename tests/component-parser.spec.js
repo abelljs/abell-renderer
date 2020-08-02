d20d831dc75e9f0ec82d4ef803b9198ef58c434f
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-02 15:37:47 +0530

    component parser almost e2e tests

--- tests/component-parser.spec.js
+++ tests/component-parser.spec.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 const expect = require('chai').expect;
 
 const {
   parseAttribute,
-  parseComponentTags
+  parseComponentTags,
+  parseComponent
 } = require('../src/component-parser.js');
 
 describe('parseAttribute()', () => {
@@ -35,7 +38,8 @@ describe('parseAttribute()', () => {
 });
 
 describe('parseComponentTags()', () => {
-  it('should turn string attributes into object', () => {
+  // eslint-disable-next-line max-len
+  it('should parse Abell Component Tag to Component Object before execution', () => {
     const code = `
     var Nav = require('./components/Nav.abell');
     <div>
@@ -44,6 +48,7 @@ describe('parseComponentTags()', () => {
           foo: 'bar'
         }
       />
+      <NotComponent/>
     </div>
     `;
 
@@ -54,10 +59,76 @@ describe('parseComponentTags()', () => {
     ).to.equal(
       `
       var Nav = require('./components/Nav.abell');
-      <div>{{ Nav({foo: 'bar'}).renderedHTML }}</div>
+      <div>{{ Nav({foo: 'bar'}).renderedHTML }} <NotComponent/></div>
       `
         .trim()
         .replace(/\s|\n|\r/g, '')
     );
+  });
+});
+
+describe('parseComponent()', () => {
+  it('should parse component and return a componentTree - Sample.abell', () => {
+    const componentTree = parseComponent(
+      path.join(__dirname, 'resources', 'Sample.abell'),
+      {
+        foo: '123TEST'
+      },
+      { filename: 'component-parser.spec.js' }
+    );
+
+    expect(componentTree.renderedHTML.trim().replace(/\n|\r|\s/g, '')).to.equal(
+      '<div>Component to test abell. 123TEST</div>'
+        .trim()
+        .replace(/\n|\r|\s/g, '')
+    );
+
+    expect(componentTree.styles[0].content).to.exist.and.include('div');
+    expect(componentTree.styles[0]).to.have.keys(
+      'component',
+      'attributes',
+      'componentPath',
+      'content'
+    );
+
+    expect(componentTree.scripts[0].content).to.exist.and.include(
+      'console.log(3)'
+    );
+
+    expect(componentTree.scripts[0]).to.have.keys(
+      'component',
+      'attributes',
+      'componentPath',
+      'content'
+    );
+  });
+
+  it('should work for nested components - Parent.abell', () => {
+    const componentTree = parseComponent(
+      path.join(__dirname, 'resources', 'Parent.abell'),
+      {
+        filename: 'component-parser.spec.js',
+        basePath: path.join(__dirname, 'resources')
+      }
+    );
+
+    expect(componentTree.renderedHTML.trim().replace(/\n|\r|\s/g, '')).to.equal(
+      '<div><div>Component to test abell. Woop Woop!</div></div>'
+        .trim()
+        .replace(/\n|\r|\s/g, '')
+    );
+
+    expect(componentTree.components[0].styles[0].content).to.exist.and.include(
+      'div'
+    );
+    expect(componentTree.components[0].styles[0]).to.have.keys(
+      'component',
+      'attributes',
+      'componentPath',
+      'content'
+    );
+
+    expect(componentTree.styles.length).to.equal(0);
+    expect(componentTree.scripts.length).to.equal(0);
   });
 });

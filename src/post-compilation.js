9860fc5e7f5b03a27dcbd22fd66700eaaa39f774
Harshit Pant <pantharshit00@gmail.com>
2020-09-11 19:28:41 +0530

    feat: add comment for prefixHtmlTags

--- src/post-compilation.js
+++ src/post-compilation.js
@@ -1,5 +1,6 @@
 const { ABELL_CSS_DATA_PREFIX } = require('./parsers/css');
 
+// This function uses a single regular expression to add a data prefix to every html opening tag passed to it
 const prefixHtmlTags = (htmlString, hash) => {
   const openingTagRegexp = /\<([a-zA-Z]+)(.*?)(\s?\/?)\>/gs;
   return htmlString.replace(
